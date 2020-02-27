dff98b956e668722facfda676db4caaaed3a1209
Mickael MAS <mickael.mas@appiwedia.com>
2020-02-27 13:03:51 +0100

    TabView

--- PeopleSwiftUI/ContentView.swift
+++ PeopleSwiftUI/ContentView.swift
@@ -11,14 +11,30 @@ struct ContentView: View {
     
     var body: some View {
         
-        NavigationView {
-            List(People.default) { people in
-                
-                NavigationLink(destination: PeopleDetailView(people: people)) {
-                    PeopleView(people: people)
-                }
-            }.navigationBarTitle("Mes potes", displayMode: .large)
+        TabView {
+            
+            PeopleListView()
+                .tabItem { VStack {
+                    Image(systemName: "person.3")
+                    Text("Mes potes")
+                    }
+            }
+            
+            Text("Favoris")
+                .tabItem { VStack {
+                    Image(systemName: "star")
+                    Text("Favoris")
+                    }
+            }
+            
+            Text("Recherche")
+                .tabItem { VStack {
+                    Image(systemName: "magnifyingglass")
+                    Text("Recherche")
+                    }
+            }
         }
+        
     }
 }
 
@@ -36,6 +52,19 @@ struct PeopleView: View {
             Image(people.imageName)
                 .clipShape(Circle())
             Text(people.name)
+        }
+    }
+}
+
+struct PeopleListView: View {
+    var body: some View {
+        NavigationView {
+            List(People.default) { people in
+                
+                NavigationLink(destination: PeopleDetailView(people: people)) {
+                    PeopleView(people: people)
+                }
+            }.navigationBarTitle("Mes potes", displayMode: .large)
         }
     }
 }
